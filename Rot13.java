6c4b1c9f2e1326a22dc83168636a53b4a673c4d9
Sergio Prieto Burrruezo <cf20sergio.prieto@iesjoandaustria.org>
2024-09-19 18:31:50 +0200

    ejercicio clase

---
+++ Rot13.java
@@ -0,0 +1,13 @@
+/*
+Ejercicio donde tenemos que crear un cifrado con 2 Arrays
+abecedario ñ y Ç
+*/
+public class Rot13 {
+    // Arrays amb les lletres de l'abecedari
+    private static final char[] minuscules = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+    private static final char[] majuscules = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+    public static String xifraRot13(String cadena) {
+        StringBuilder resultat = new StringBuilder();
+        
+        }
+}
