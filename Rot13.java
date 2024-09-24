5eb25e43ab053cfa0ef2ee82002660400d1ecf7c
Sergio Prieto Burrruezo <cf20sergio.prieto@iesjoandaustria.org>
2024-09-24 19:10:47 +0200

    ejercicios

--- Rot13.java
+++ Rot13.java
@@ -1,13 +1,62 @@
 /*
-Ejercicio donde tenemos que crear un cifrado con 2 Arrays
-abecedario ñ y Ç
+Programa que consiste en sustituir cadena de letras por la que este en la 13
+posición
+IndexOF busca un caracter a partir de un indice especifico, si no lo encuentra
+devuelve -1.
 */
+import java.util.Scanner;
+
 public class Rot13 {
-    // Arrays amb les lletres de l'abecedari
-    private static final char[] minuscules = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
-    private static final char[] majuscules = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+    private static final String MINUSCULAS = "abcdefghijklmnopqrstuvwxyz";
+    private static final String MAYUSCULAS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     public static String xifraRot13(String cadena) {
-        StringBuilder resultat = new StringBuilder();
-        
+        String resultado = "";
+        for (int i = 0; i < cadena.length(); i++) {
+            char c = cadena.charAt(i);
+            if (MINUSCULAS.indexOf(c) != -1) {
+                int pos = MINUSCULAS.indexOf(c);
+                resultado += MINUSCULAS.charAt((pos + 13) % 26);
+            } else if (MAYUSCULAS.indexOf(c) != -1) {
+                int pos = MAYUSCULAS.indexOf(c);
+                resultado += MAYUSCULAS.charAt((pos + 13) % 26);
+            } else {
+                resultado += c;
+            }
         }
+        return resultado;
+    }
+
+    public static String desxifraRot13(String cadena) {
+        String resultado = "";
+        for (int i = 0; i < cadena.length(); i++) {
+            char c = cadena.charAt(i);
+            if (MINUSCULAS.indexOf(c) != -1) {
+                int pos = MINUSCULAS.indexOf(c);
+                resultado += MINUSCULAS.charAt((pos + 13) % 26);
+            } else if (MAYUSCULAS.indexOf(c) != -1) {
+                int pos = MAYUSCULAS.indexOf(c);
+                resultado += MAYUSCULAS.charAt((pos + 13) % 26);
+            } else {
+                resultado += c;
+            }
+        }
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce una palabra para codificarla");
+        String texto = scanner.nextLine();
+        if(texto.isBlank()) { 
+            System.out.println("Texto no valido");
+        } else {
+            String cifrado = xifraRot13(texto);
+            String descifrado = desxifraRot13(cifrado);
+
+            System.out.println("Original: " + texto);
+            System.out.println("Cifrado: " + cifrado);
+            System.out.println("Descifrado: " + descifrado);
+        }
+    }
 }
